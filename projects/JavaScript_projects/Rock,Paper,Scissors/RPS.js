0cfbd70d65c08cb3dd58c1bd9e9b54c0ddfe8085
saigel <sacuevas27@gmail.com>
2025-07-12 21:49:50 -0400

    Subiendo código en JS

--- projects/JavaScript_projects/Rock,Paper,Scissors/RPS.js
+++ projects/JavaScript_projects/Rock,Paper,Scissors/RPS.js
@@ -5,5 +5,5 @@
 3. Compara ambos valores tomando en cuenta el tema de las victorias.
 4. Declara al ganador.*/
 
-prompt()
-console.log("Seleccione entre 1. Piedra, 2. Papel o 3. Tijera(introduzca solo el nombre de la opción)")
+
+console.log("Hello World")
