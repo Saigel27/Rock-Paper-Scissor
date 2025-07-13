6f4f6436f99018d058b1ae8625a6af6e66a1a508
saigel <sacuevas27@gmail.com>
2025-07-12 21:40:26 -0400

    Primera prueba de funcionamiento de commit

---
+++ projects/JavaScript_projects/Rock,Paper,Scissors/RPS.js
@@ -0,0 +1,9 @@
+//Juego de papel, piedras o tijeras.
+/*Consiste de varios pasos:
+1. Recibe la entrada del participante.
+2. Toma un valor aleatorio de la computadora.
+3. Compara ambos valores tomando en cuenta el tema de las victorias.
+4. Declara al ganador.*/
+
+prompt()
+console.log("Seleccione entre 1. Piedra, 2. Papel o 3. Tijera(introduzca solo el nombre de la opción)")
